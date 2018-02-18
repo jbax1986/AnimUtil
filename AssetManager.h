ef81c869329465364c03eb4df3dca37f391926ae
jbax1986 <35898548+jbax1986@users.noreply.github.com>
2018-02-18 04:15:51 -0800

    version 1.00

---
+++ AssetManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef ASSET_MANAGER_H
+#define ASSET_MANAGER_H
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+class AssetManager
+{
+public:
+	AssetManager();
+
+	static sf::Texture& GetTexture(std::string const& filename);
+private:
+	std::map<std::string, sf::Texture>m_Textures;
+
+	static AssetManager* sInstance;
+};
+#endif
