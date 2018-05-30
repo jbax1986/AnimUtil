deb98ce4c3fe15adaefc1259657aae97edffe8ab
jbax1986 <35898548+jbax1986@users.noreply.github.com>
2018-05-30 12:32:28 -0700

    Update Animator.h

--- Animator.h
+++ Animator.h
@@ -1,19 +1,16 @@
 #pragma once
-
 #ifndef ANIMATOR_H
 #define AMIMATOR_H
 
 #include <vector>
 #include <list>
-
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/System/Time.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 
-class Animator
+class Animator  //or the denseness as i call it.
 {
 public:
-
 	struct Animation
 	{
 		std::string m_Name;
@@ -21,39 +18,36 @@ public:
 		std::vector<sf::IntRect> m_Frames;
 		sf::Time m_Duration;
 		bool m_Looping;
-
-	Animation(std::string const& name, std::string const& textureName, sf::Time const& duration, bool looping): m_Name(name), m_TextureName(textureName), m_Duration(duration), m_Looping(looping) { }
-
-	void AddFrames(sf::Vector2i const& startFrom, sf::Vector2i const& frameSize, unsigned int frames)
-	{
-		sf::Vector2i current = startFrom;
-		for (unsigned int i = 0; i < frames; i++)
+		Animation(std::string const& name,
+			  std::string const& textureName,
+			  sf::Time const& duration,
+			  bool looping) :
+			  m_Name(name),
+			  m_TextureName(textureName),
+			  m_Duration(duration),
+			  m_Looping(looping) 
+		{ }
+		void AddFrames(sf::Vector2i const& startFrom, sf::Vector2i const& frameSize, unsigned int frames)
+		{
+			sf::Vector2i current = startFrom;
+			for (unsigned int i = 0; i < frames; i++)
 			{
 				m_Frames.push_back(sf::IntRect(current.x, current.y, frameSize.x, frameSize.y));
 				current.x += frameSize.x;
 			}
 		}
 	};
-
 	Animator(sf::Sprite& sprite);
-
 	Animator::Animation& CreateAnimation(std::string const& name, std::string const& textureName, sf::Time const& duration, bool loop = false);
-
 	void Update(sf::Time const& dt);
-
 	bool SwitchAnimation(std::string const& name);
-
 	std::string GetCurrentAnimationName() const;
-
 private:
 	Animator::Animation* FindAnimation(std::string const& name);
-
 	void SwitchAnimation(Animator::Animation* animation);
-
 	sf::Sprite& m_Sprite;
 	sf::Time m_CurrentTime;
 	std::list<Animator::Animation> m_Animations;
 	Animator::Animation* m_CurrentAnimation;
 };
-
 #endif
